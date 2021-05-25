1005da087c4869bfc0fa052325e1e3558b9dd3f5
jaharaclark <jaharamclark@gmail.com>
2021-05-25 08:33:30 -1000

    add validations and relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,10 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :status
+
+  belongs_to :customer
+  belongs_to :merchant
+
+  has_many :invoice_items
+  has many :items, through: :invoice_items
+  has_many :transactions
 end
