13e6c5839aa2ab4e737dff9fe09cf96d831acc35
jaharaclark <jaharamclark@gmail.com>
2021-05-26 18:05:54 -1000

    Complete section one testing

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Items API' do 
-  it 'gets all items, a maximum of 20 at a time' do
-    create_list(:item, 20)
-    
+  before :each do
+    @merchant_1 = create(:merchant)
+    @item_1 = create(:item, merchant: @merchant_1)
+    create_list(:item, 80)
+  end
+
+  #Get All Items
+  it 'happy: gets all items, a maximum of 20 at a time' do
     get '/api/v1/items'
     
     expect(response).to be_successful
     
     items = JSON.parse(response.body, symbolize_names: true)[:data]
-
     expect(items.count).to eq(20)
 
     items.each do |item|
@@ -26,10 +30,32 @@ RSpec.describe 'Items API' do
     end
   end
 
-  it 'can get one item by its id' do
-    id = create(:item).id
+  it 'happy: fetches second page of 20 items' do
+    get '/api/v1/items?per_page=20&page=2'
 
-    get "/api/v1/items/#{id}"
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(items.count).to eq(20)      
+  end
+  
+  it 'sad: fetching page 1 if page is 0 or lower' do
+    get '/api/v1/items?per_page=20&page=0'
+    expect(response).to be_successful
+    
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(items.count).to eq(20)
+
+    get '/api/v1/items?per_page=20&page=-7'
+    expect(response).to be_successful
+    
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(items.count).to eq(20)
+  end
+  
+#Get One Item
+  it 'happy: can get one item by its id' do
+    get "/api/v1/items/#{@item_1.id}"
 
     item = JSON.parse(response.body, symbolize_names: true)[:data]
 
@@ -47,13 +73,19 @@ RSpec.describe 'Items API' do
     expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
 
-  it 'can create a new item' do
-    merchant = create(:merchant)
+  it 'sad: bad item integer id returns 404' do
+    get "/api/v1/items/1364648843"
+    expect(response).not_to be_successful
+    expect(response.status).to eq(404)
+  end
+
+  #Create (then Delete) One Item
+  it 'happy: can create a new item' do
     item_params = ({
                     name: 'Sticky Note',
                     description: 'a place to write thoughts',
                     unit_price: 3.98,
-                    merchant_id: merchant.id
+                    merchant_id: @merchant_1.id
                   })
 
     headers = {"CONTENT_TYPE" => "application/json"}
@@ -65,16 +97,35 @@ RSpec.describe 'Items API' do
     expect(response).to be_successful
     expect(created_item[:name]).to eq(item_params[:name])
   end
-  
-  it 'can get an items merchant' do
-    merchant = create(:merchant)
-    item = create(:item, merchant: merchant)
 
-    get "/api/v1/items/#{item.id}/merchant"
+  #Update One Item
+  it 'edge: string item id returns 404' do
+    item_id_string = "3515"
+    get "/api/v1/items/#{item_id_string}"
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).not_to be_successful
+    expect(response.status).to eq(404)
+    expect(item).to eq(nil)
+  end
+
+  #Get an Item's Merchant
+  it 'happy: can get an items merchant' do
+    get "/api/v1/items/#{@item_1.id}/merchant"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(response).to be_successful
+    expect(merchant[:id].to_i).to eq(@merchant_1.id)
+  end
+
+    it 'sad: bad merchant integer id returns 404 and no items' do
+    get "/api/v1/items/16843187516853"
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(404)
+    expect(item).to eq(nil)
   end
 
 end
